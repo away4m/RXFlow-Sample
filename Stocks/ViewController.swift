b47243aea035cddff1109239de3c86d219a177ff
Ali Kiran <away4m@gmail.com>
2020-04-11 01:46:19 +0200

    Initial Commit

---
+++ Stocks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by ALI KIRAN on 11.04.20.
+//  Copyright © 2020 ALI KIRAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
